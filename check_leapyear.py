7f24766b3cf941f903f3d9e974d1c3128ce23c34
priya poosaala <priyapoosaala@gmail.com>
2025-01-12 15:06:01 +0530

    Add files via upload

--- check_leapyear.py
+++ check_leapyear.py
@@ -1,12 +1,8 @@
 year = int(input("Enter year: "))
 is_leap_year = False
 
-if year % 4 == 0:
-    is_leap_year = True
-    if year % 100 == 0:
-        if year % 400 == 0:
-            is_leap_year = True
-        else:
-            is_leap_year = False
+if (year % 4 == 0 and year%100!=0) or (year%400==0):
+    is_leap_year=True
+    
 
 print(is_leap_year)
